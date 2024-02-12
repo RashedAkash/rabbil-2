a01215126d38dd19c31ec3700860ad8111e535bd
hablu <131282586+hablu@users.noreply.github.com>
2024-02-12 12:14:37 +0600

    server data added

--- src/app/server/page.js
+++ src/app/server/page.js
@@ -1,11 +1,23 @@
-import React from 'react';
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
 
-const page = () => {
-  return (
-    <div>
-      server
-    </div>
-  );
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const page = async() => {
+  const data = await getData();
+  console.log(data);
+  return <div>
+    server
+    <h1>{JSON.stringify(data) }</h1>
+  </div>;
 };
 
 export default page;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div>
-      <ul>
+      <ul className=' flex gap-5'>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/swr">Swr</a></li>
