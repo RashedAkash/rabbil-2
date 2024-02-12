f046aad84d3e89cd529913d6549d7f4378de1656
hablu <131282586+hablu@users.noreply.github.com>
2024-02-12 12:22:23 +0600

    client data added

--- src/app/client/page.js
+++ src/app/client/page.js
@@ -1,9 +1,10 @@
+import Client from '@/components/Client';
 import React from 'react';
 
 const page = () => {
   return (
     <div>
-      client
+      <Client />
     </div>
   );
 };
---
+++ src/components/Client.js
@@ -0,0 +1,21 @@
+"use client"
+
+import { data } from "autoprefixer";
+import { useEffect, useState } from "react";
+
+const Client = () => {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  },[])
+  return (
+    <div>
+      <h1>Client</h1>
+      <h1>{JSON.stringify(user) }</h1>
+    </div>
+  );
+};
+
+export default Client;
