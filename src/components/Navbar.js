d83b89e659c7450fb19382b47a142a624683f3c5
hablu <131282586+hablu@users.noreply.github.com>
2024-02-12 12:05:17 +0600

    navbar create

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/swr">Swr</a></li>
+        <li><a href="client">client</a></li>
+        <li><a href="server">server</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
