23e27a20b22aab8deac1c3c9b4a2278a25a685cc
hablu <131282586+hablu@users.noreply.github.com>
2024-02-12 12:30:50 +0600

    swr added

--- src/app/swr/page.js
+++ src/app/swr/page.js
@@ -1,11 +1,19 @@
-import React from 'react';
+"use client"
+
+import { fetcher } from "@/components/fetcher";
+import useSWR from "swr";
 
 const page = () => {
-  return (
-    <div>
-      swr
-    </div>
-  );
+    const { data, error, isLoading } = useSWR(
+      "https://jsonplaceholder.typicode.com/users",
+      fetcher
+    );
+
+    if (error) return <div>failed to load</div>;
+    if (isLoading) return <div>loading...</div>;
+
+    // render data
+    return <div>hello {data.length}!</div>;
 };
 
 export default page;
