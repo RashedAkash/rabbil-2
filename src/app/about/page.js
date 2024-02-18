4a35d2bafdd7a1820cdc3fd355d09cee1d306f76
hablu <131282586+hablu@users.noreply.github.com>
2024-02-18 14:01:48 +0600

    about img create

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 async function getData() {
   try {
     const res1 = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -21,13 +23,27 @@ async function getData() {
 const page = async() => {
   const data = await getData()
  
-  return <div>about
-    <h1>{data['data1'].length }</h1>
-    <h1>{data['data2'].length }</h1>
-    <h1>{data['data3'].length }</h1>
-    <h1>{data['data4'].length }</h1>
-  
-  </div>;
+  return (
+    <div>
+      about
+      <h1>{data["data1"].length}</h1>
+      <h1>{data["data2"].length}</h1>
+      <h1>{data["data3"].length}</h1>
+      <h1>{data["data4"].length}</h1>
+      <Image
+        alt="pic"
+        src="https://plus.unsplash.com/premium_photo-1706625698049-02ddcd91361d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        width={400}
+        height={300}
+      />
+      <Image
+        alt="res"
+        width={500}
+        height={400}
+        src="https://images.unsplash.com/photo-1696388790726-68161096266e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+    </div>
+  );
 };
 
 export default page;
