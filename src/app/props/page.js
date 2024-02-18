4a49a11a0b5ef4a14bbe1d73f5d843227d4a237b
hablu <131282586+hablu@users.noreply.github.com>
2024-02-18 07:45:25 +0600

    freepic added

--- src/app/props/page.js
+++ src/app/props/page.js
@@ -25,6 +25,12 @@ const page = async () => {
         height={500}
         src="https://images.unsplash.com/photo-1637214599674-8bd83beb260b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
+      <Image
+        src="https://img.freepik.com/free-photo/mesmerizing-scenery-green-mountains-with-cloudy-sky-surface_181624-27189.jpg?w=1060&t=st=1708220506~exp=1708221106~hmac=f4cc24afc4aca8485a877eaa97e3b448db72c8c02b7f59e9dbe72016c495e1a6"
+        width={500}
+        height={500}
+        alt="free"
+      />
     </div>
   );
 };
