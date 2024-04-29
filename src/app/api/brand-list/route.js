877a7d6ba7fd8f6683400be39e7f54915b945e39
hablu <131282586+hablu@users.noreply.github.com>
2024-04-29 08:10:21 +0600

    prisma migrate complete

---
+++ src/app/api/brand-list/route.js
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+const { data } = require("autoprefixer");
+const { NextResponse } = require("next/server");
+
+export const GET = async (req, res) => {
+  try {
+    const prisma = new PrismaClient();
+    const result = await prisma.brands.findMany();
+    return NextResponse.json({ status: true, data: result });
+  } catch (error) {
+    return NextResponse.json({ status: false, data: error });
+  }
+};
